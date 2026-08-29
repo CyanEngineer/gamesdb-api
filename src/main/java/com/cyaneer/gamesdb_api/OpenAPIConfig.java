af922fa1514a2e51e8233c3241013602fdb96fcc
CyanEngineer <hello@cyaneer.com>
2026-08-29 23:50:20 +0200

    Automatically matching the pom and OpenAPI version numbers

--- src/main/java/com/cyaneer/gamesdb_api/OpenAPIConfig.java
+++ src/main/java/com/cyaneer/gamesdb_api/OpenAPIConfig.java
@@ -1,5 +1,7 @@
 package com.cyaneer.gamesdb_api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,13 +10,17 @@ import io.swagger.v3.oas.models.info.Info;
 
 @Configuration
 public class OpenAPIConfig {
+
+    @Autowired(required = false)
+    private BuildProperties buildProperties;
     
     @Bean
     public OpenAPI gamesDBOpenAPI() {
+        String version = buildProperties != null ? buildProperties.getVersion() : "dev";
         return new OpenAPI()
             .info(new Info()
                 .title("GamesDB API")
-                .version("1.3.3")
+                .version(version)
                 .description("RESTful API for games I have played or want to play")
             );
     }
